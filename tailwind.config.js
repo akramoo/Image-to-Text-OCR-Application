87d8b9d5d7254124d9814706131d418b47afb9ab
akramoo <samirounis2@gmail.com>
2024-06-01 02:48:23 +0100

    fixing the path of the root file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js,jsx}"],
+  content: ["./frontend/**/*.{html,js,jsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
